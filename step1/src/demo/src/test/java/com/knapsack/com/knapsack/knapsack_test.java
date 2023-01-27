06f6f4a94df6ae5094c5587accc4eb64bf240207
israelrizo <israel.lopezrizo@gmail.com>
2023-01-27 17:31:39 -0600

    CommitStep1_Step2

---
+++ step1/src/demo/src/test/java/com/knapsack/com/knapsack/knapsack_test.java
@@ -0,0 +1,79 @@
+package com.knapsack;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class knapsack_test extends step1Knapsack
+{
+    
+    @Test
+    public void Test_Default()
+    {
+        int weight= 10;
+        int items_weight[] = {3, 6, 8};
+        int items_values[] = {50, 60, 100};
+        int expected = 110;
+        int result = knapsack(weight, items_weight, items_values);
+        System.out.println("Test 1:"+result);
+        assertEquals( expected, result);
+    }
+    @Test
+    public void Test_MoreItemList()
+    {
+        int weight= 10;
+        int items_weight[] = {3, 6, 8, 1, 2, 5};
+        int items_values[] = {50, 60, 100, 10, 35, 120};
+        int expected = 205;
+        int result = knapsack(weight, items_weight, items_values);
+        System.out.println("Test 2:"+result);
+        assertEquals( expected, result);
+    }
+    @Test
+    public void Test_MoreWeight()
+    {
+        int weight= 15;
+        int items_weight[] = {3, 6, 8, 1, 2, 5};
+        int items_values[] = {50, 60, 100, 10, 35, 120};
+        int expected = 240;
+        int result = knapsack(weight, items_weight, items_values);
+        System.out.println("Test 3:"+result);
+        assertEquals( expected, result);
+    }
+    @Test
+    public void Test_LessWeight()
+    {
+        int weight= 8;
+        int items_weight[] = {3, 6, 8, 1, 2, 5};
+        int items_values[] = {50, 60, 100, 10, 35, 120};
+        int expected = 170;
+        int result = knapsack(weight, items_weight, items_values);
+        System.out.println("Test 4:"+result);
+        assertEquals( expected, result);
+    }
+
+    @Test
+    public void Test_DuplicatedItemsValue()
+    {
+        int weight= 10;
+        int items_weight[] = {3, 6, 8, 1};
+        int items_values[] = {50, 60, 100, 50};
+        int expected = 160;
+        int result = knapsack(weight, items_weight, items_values);
+        System.out.println("Test 5:"+result);
+        assertEquals( expected, result);
+    }
+
+    @Test
+    public void Test_LongerItemsList() 
+    {
+        int weight= 10;
+        int items_weight[] = {1, 3, 4, 6, 8};
+        int items_values[] = {20,40,50, 60, 100};
+        int expected = 120;
+        int result = knapsack(weight, items_weight, items_values);
+        System.out.println("Test 6:"+result);
+        assertEquals( expected, result);
+    }
+}
